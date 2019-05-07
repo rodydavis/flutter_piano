6d3aa4915941588537040c8f3d4613b55cd8c574
Rody Davis <rody.davis.jr@gmail.com>
2019-05-07 14:43:20 -0400

    Adding PP

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'The Pocket Piano',
       theme: ThemeData.dark(),
       home: HomeScreen(),
