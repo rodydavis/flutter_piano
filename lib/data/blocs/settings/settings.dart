43b83cd6cda47f2e777b082d62e9db8e3e049953
Rody Davis <rody.davis.jr@gmail.com>
2019-09-30 08:09:49 -0400

    settings update

--- lib/data/blocs/settings/settings.dart
+++ lib/data/blocs/settings/settings.dart
@@ -5,8 +5,19 @@ class Settings {
     this.darkMode = false,
     this.useSystemSetting = true,
     this.locale = const Locale("en", "US"),
+    this.widthRatio = 0.5,
+    this.showLabels = true,
+    this.labelsOnlyOctaves = false,
+    this.disableScroll = false,
+    this.shouldVibrate = true,
   });
 
-  bool darkMode, useSystemSetting;
+  bool darkMode,
+      useSystemSetting,
+      showLabels,
+      labelsOnlyOctaves,
+      disableScroll,
+      shouldVibrate;
   Locale locale;
+  double widthRatio;
 }
--- lib/ui/home/screen.dart
+++ lib/ui/home/screen.dart
@@ -2,8 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_midi/flutter_midi.dart';
-import 'package:localstorage/localstorage.dart';
+import 'package:flutter_piano/data/blocs/blocs.dart';
 
 import '../../generated/i18n.dart';
 import '../../plugins/app_review/app_review.dart';
@@ -18,8 +19,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
-  final LocalStorage _storage = new LocalStorage('app_settings');
-
   @override
   initState() {
     _loadSoundFont();
@@ -34,7 +33,6 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
     rootBundle.load("assets/sounds/Piano.sf2").then((sf2) {
       FlutterMidi.prepare(sf2: sf2, name: "Piano.sf2");
     });
-    _loadSettings();
     VibrateUtils.canVibrate.then((vibrate) {
       if (mounted)
         setState(() {
@@ -43,123 +41,111 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
     });
   }
 
-  void _loadSettings() async {
-    await _storage.ready;
-    if (mounted)
-      setState(() {
-        _widthRatio = _storage.getItem("ratio") ?? 0.5;
-        _showLabels = _storage.getItem("labels") ?? true;
-        _labelsOnlyOctaves = _storage.getItem("octaves") ?? false;
-        _disableScroll = _storage.getItem("scroll") ?? false;
-        shouldVibrate = _storage.getItem("vibrate") ?? true;
-      });
-  }
-
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     print("State: $state");
     _loadSoundFont();
   }
 
-  double get keyWidth => 40 + (80 * (_widthRatio ?? 0.5));
-  double _widthRatio;
-  bool _showLabels = true;
-  bool _labelsOnlyOctaves = true;
-  bool _disableScroll = false;
   bool canVibrate = false;
-  bool shouldVibrate = true;
 
   @override
   Widget build(BuildContext context) {
-    print(MediaQuery.of(context).size);
-    return Scaffold(
-      drawer: Drawer(
-          child: SafeArea(
-        child: ListView(children: <Widget>[
-          Container(height: 20.0),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text("Settings"),
-            onTap: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => SettingsScreen()));
-            },
+    // print(MediaQuery.of(context).size);
+    return BlocBuilder<SettingsBloc, SettingsState>(
+      builder: (context, state) => Scaffold(
+        drawer: Drawer(
+            child: SafeArea(
+          child: ListView(children: <Widget>[
+            Container(height: 20.0),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Settings"),
+              onTap: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => SettingsScreen()));
+              },
+            ),
+            Divider(),
+            if (state is SettingsReady) ...[
+              ListTile(title: Text("Change Width")),
+              Slider(
+                  activeColor: Colors.redAccent,
+                  inactiveColor: Colors.white,
+                  min: 0.0,
+                  max: 1.0,
+                  value: state.settings.widthRatio,
+                  onChanged: (double value) {
+                    if (mounted) setState(() => _widthRatio = value);
+                    _storage.setItem("ratio", value);
+                  }),
+              Divider(),
+              ListTile(
+                  title: Text("Show Labels"),
+                  trailing: Switch(
+                      value: state.settings.showLabels,
+                      onChanged: (bool value) {
+                        if (mounted) setState(() => _showLabels = value);
+                        _storage.setItem("labels", value);
+                      })),
+              Container(
+                child: _showLabels
+                    ? ListTile(
+                        title: Text("Only For Octaves"),
+                        trailing: Switch(
+                            value: state.settings.labelsOnlyOctaves,
+                            onChanged: (bool value) {
+                              if (mounted)
+                                setState(() => _labelsOnlyOctaves = value);
+                              _storage.setItem("octaves", value);
+                            }))
+                    : null,
+              ),
+              Divider(),
+              ListTile(
+                  title: Text("Disable Scroll"),
+                  trailing: Switch(
+                      value: state.settings.disableScroll,
+                      onChanged: (bool value) {
+                        if (mounted) setState(() => _disableScroll = value);
+                        _storage.setItem("scroll", value);
+                      })),
+              Divider(),
+              Container(
+                child: canVibrate
+                    ? ListTile(
+                        title: Text("Key Feedback"),
+                        trailing: Switch(
+                            value: state.settings.shouldVibrate,
+                            onChanged: (bool value) {
+                              if (mounted)
+                                setState(() => shouldVibrate = value);
+                              _storage.setItem("vibrate", value);
+                            }))
+                    : null,
+              ),
+            ],
+          ]),
+        )),
+        appBar: AppBar(
+          title: Text(
+            I18n.of(context).title,
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 30.0,
+            ),
           ),
-          Divider(),
-          ListTile(title: Text("Change Width")),
-          Slider(
-              activeColor: Colors.redAccent,
-              inactiveColor: Colors.white,
-              min: 0.0,
-              max: 1.0,
-              value: _widthRatio ?? 0.5,
-              onChanged: (double value) {
-                if (mounted) setState(() => _widthRatio = value);
-                _storage.setItem("ratio", value);
-              }),
-          Divider(),
-          ListTile(
-              title: Text("Show Labels"),
-              trailing: Switch(
-                  value: _showLabels,
-                  onChanged: (bool value) {
-                    if (mounted) setState(() => _showLabels = value);
-                    _storage.setItem("labels", value);
-                  })),
-          Container(
-            child: _showLabels
-                ? ListTile(
-                    title: Text("Only For Octaves"),
-                    trailing: Switch(
-                        value: _labelsOnlyOctaves,
-                        onChanged: (bool value) {
-                          if (mounted)
-                            setState(() => _labelsOnlyOctaves = value);
-                          _storage.setItem("octaves", value);
-                        }))
-                : null,
-          ),
-          Divider(),
-          ListTile(
-              title: Text("Disable Scroll"),
-              trailing: Switch(
-                  value: _disableScroll,
-                  onChanged: (bool value) {
-                    if (mounted) setState(() => _disableScroll = value);
-                    _storage.setItem("scroll", value);
-                  })),
-          Divider(),
-          Container(
-            child: canVibrate
-                ? ListTile(
-                    title: Text("Key Feedback"),
-                    trailing: Switch(
-                        value: shouldVibrate,
-                        onChanged: (bool value) {
-                          if (mounted) setState(() => shouldVibrate = value);
-                          _storage.setItem("vibrate", value);
-                        }))
-                : null,
-          ),
-        ]),
-      )),
-      appBar: AppBar(
-        title: Text(
-          I18n.of(context).title,
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 30.0,
-          ),
+          actions: <Widget>[
+            DarkModeToggle(),
+          ],
         ),
-        actions: <Widget>[
-          DarkModeToggle(),
-        ],
+        body: _buildKeys(context),
       ),
-      body: _buildKeys(context),
     );
   }
 
   Widget _buildKeys(BuildContext context) {
+    double = keyWidth = 40 + (80 * (_widthRatio ?? 0.5));
     final _vibrate = shouldVibrate && canVibrate;
     if (MediaQuery.of(context).size.height > 600) {
       return Flex(
