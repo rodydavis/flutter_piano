f80fa86c57ee5162484a40f0b44280b5d64f06c2
Rody Davis <rody.davis.jr@gmail.com>
2019-10-15 14:48:48 -0400

    beta

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -2,9 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/usr/local/Caskroom/flutter/1.2.1/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/rodydavis/Development/github/flutter_piano"
-export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/usr/local/Caskroom/flutter/1.2.1/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_FRAMEWORK_DIR=/usr/local/Caskroom/flutter/1.2.1/flutter/bin/cache/artifacts/engine/darwin-x64"
 export "FLUTTER_BUILD_NAME=1.0.3"
 export "FLUTTER_BUILD_NUMBER=45"
